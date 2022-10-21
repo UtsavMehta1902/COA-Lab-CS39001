44dd8ce40345c596b56416a20526e02f06f18917
Utsav Mehta <utsavmehta2002@yahoo.com>
2022-10-21 14:59:31 +0530

    Added code for ALU.

--- Assignment-6/ALU_unit.v
+++ Assignment-6/ALU_unit.v
@@ -24,9 +24,49 @@ module ALU_unit(a, b, ALUsel, ALUop, carry, zero, sign, result);
     mux_32_to_1 mux1 (.a(a), .b(32'b00000000000000000000000000000001), .select(ALUsel), .out(out_mux1));
     mux_32_to_1 mux2 (.a(b), .b(out_not), .select(ALUsel), .out(out_mux2));
 
-    adder_32_bit adder (.a(out_mux1), .b(out_mux2), .c_in(1'b0), c_out(carry), .sum(out_adder));
+    adder_32_bit adder (.a(out_mux1), .b(out_mux2), .c_in(1'b0), c_out(temp_carry), .sum(out_adder));
 
-    shifter shift (.in())
+    shifter shift (.in(out_mux1), .shamt(out_mux2), .dir(ALUop[1]), .arith_or_logic(ALUop[0]), .out(out_shifter));
 
+    assign out_not = ~b;
+    assign out_and = out_mux1 & out_mux2;
+    assign out_xor = out_mux1 ^ out_mux2;
 
+    always @(a or b or ALUsel or ALUop) begin
+        
+        if(ALUop == 5'b00000)
+            result = out_mux1;
+        else if (ALUop == 5'b00001) begin
+            carry = temp_carry;
+            result = out_adder;
+        end
+        else if (ALUop == 5'b00101)
+            result = out_adder;
+        else if (ALUop == 5'b10101)
+            result = out_adder;
+        else if (ALUop == 5'b00010)
+            result = out_and;
+        else if (ALUop == 5'b00011)
+            result = out_xor;
+        else if (ALUop[4:2] == 3'b010)
+            result = out_shifter;
+        else
+            result = 32'b00000000000000000000000000000000;
+
+    end
+
+    always @(result) begin
+
+        if(result == 32'b00000000000000000000000000000000)
+            zero = 1'b0;
+        else
+            zero = 1'b1;
+
+        if(result[31])
+            sign = 1'b1;
+        else
+            sign = 1'b0;
+
+    end
+    
 endmodule
